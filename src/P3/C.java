ce25b7b1578326143c3a0b5a16f21a18265795b2
Soraza <64122420102@g.lpru.ac.th>
2022-07-01 15:32:50 +0700

    first commit

---
+++ src/P3/C.java
@@ -0,0 +1,11 @@
+package P3;
+
+public class C {
+    public static void main(String args[]) {
+        String str1 ="Good";
+        String str2 ="Good";
+        System.out.println("str1: " + str1);
+        System.out.println("str2: " + str2);
+        System.out.println("str1 == str : " + str1.equals(str2));
+    }
+}
---
+++ src/P6/F.java
@@ -0,0 +1,13 @@
+package P6;
+
+public class F {
+    public static void main(String[] args) {
+        float x = 23.5f, y = 7.35f;
+        System.out.println("x= "+x);
+        System.out.println("y= "+y);
+        System.out.println("x+y= "+ (x+y));
+        System.out.println("x-y= "+ (x-y));
+        System.out.println("x*y= "+ (x*y));
+        System.out.println("x/y= "+ (x/y));
+    }
+}
